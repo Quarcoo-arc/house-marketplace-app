53ea04196c6f1d5a87006486061d8e51f95d3d40
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-02-18 04:12:16 +0000

    edited explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,5 +1,39 @@
+import { Link } from "react-router-dom";
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
+
 const Explore = () => {
-  return <div>Explore</div>;
+  return (
+    <div className="explore">
+      <header>
+        <p className="pagHeader">Explore</p>
+      </header>
+
+      <main>
+        {/* Slider */}
+
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img
+              className="exploreCategoryImage"
+              src={rentCategoryImage}
+              alt="rent"
+            />
+            <p className="exploreCategoryName">Places for rent</p>
+          </Link>
+          <Link to="/category/sell">
+            <img
+              className="exploreCategoryImage"
+              src={sellCategoryImage}
+              alt="sell"
+            />
+            <p className="exploreCategoryName">Places for sale</p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Explore;
